f7a7b0ec6dd9c2b8bd566b7206d2372b9630f1a5
ankiitt-07 <ak077636@gmail.com>
2025-02-15 10:08:54 +0530

    day 3 Level 1 modification

--- 03 - Arrays/Level1/NumberCheck.java
+++ 03 - Arrays/Level1/NumberCheck.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class NumberCheck {
     public static void main(String[] args) {
-          Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int[] numbers = new int[5];
 
         for (int i = 0; i < numbers.length; i++) {
