efcfab729667dd508c1e2229fb7113444acb127e
ankiitt-07 <ak077636@gmail.com>
2025-02-15 10:08:05 +0530

    day 3 Level 1 solutions added

--- 03 - Arrays/Level1/MultiplicationTableRange.java
+++ 03 - Arrays/Level1/MultiplicationTableRange.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MultiplicationTableRange {
     public static void main(String[] args) {
-               Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
         int[] multiplicationResult = new int[4];
 
