400ad8d7b68fa8f85509e594902ea1399fa081a5
ankiitt-07 <ak077636@gmail.com>
2025-02-18 10:20:36 +0530

    Some changes

--- 02 - Control Flow/Level1/EmployeeeBonus.java
+++ 02 - Control Flow/Level1/EmployeeeBonus.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EmployeeBonus {
+public class EmployeeeBonus {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
--- 02 - Control Flow/Level1/MultiplicationTablee.java
+++ 02 - Control Flow/Level1/MultiplicationTablee.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MultiplicationTable {
+public class MultiplicationTablee {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
