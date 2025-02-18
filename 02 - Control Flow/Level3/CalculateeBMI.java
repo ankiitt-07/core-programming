68fe9790b703f85eda86fef445e49fef636d4190
ankiitt-07 <ak077636@gmail.com>
2025-02-18 10:22:24 +0530

    Some changes

--- 02 - Control Flow/Level3/CalculateeBMI.java
+++ 02 - Control Flow/Level3/CalculateeBMI.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CalculateBMI {
+public class CalculateeBMI {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the weight in kg : ");
