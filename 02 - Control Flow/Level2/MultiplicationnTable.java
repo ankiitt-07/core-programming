e97d27c1220244ead211171ae694e8e3efffd6f8
ankiitt-07 <ak077636@gmail.com>
2025-02-18 10:21:34 +0530

    Some modification

--- 02 - Control Flow/Level2/MultiplicationnTable.java
+++ 02 - Control Flow/Level2/MultiplicationnTable.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MultiplicationTable {
+public class MultiplicationnTable {
   public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
       System.out.print("Enter the number : ");
